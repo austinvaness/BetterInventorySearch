6d8dd3d0d791cdbe828e82fa151cd19f67ec17ec
austinvaness <austinvaness@gmail.com>
2021-06-27 16:06:47 -0500

    Changed graphic, bug fixes

--- BetterInventorySearch/Main.cs
+++ BetterInventorySearch/Main.cs
@@ -1,12 +1,8 @@
 ﻿using HarmonyLib;
 using Sandbox.Definitions;
-using Sandbox.Game.Entities;
-using Sandbox.Game.Gui;
 using Sandbox.Game.Screens.Helpers;
 using Sandbox.Graphics.GUI;
-using Sandbox.ModAPI;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using VRage.Game.Entity;
@@ -16,6 +12,9 @@ namespace avaness.BetterInventorySearch
 {
     public class Main : IPlugin
     {
+        private static FieldInfo searchBoxL, searchBoxR;
+        private static FieldInfo ownersControlL, ownersControlR;
+
         public void Dispose()
         { }
 
@@ -23,25 +22,52 @@ namespace avaness.BetterInventorySearch
         {
             Harmony harmony = new Harmony("avaness.BetterInventorySearch");
 
-            Type originalType = AccessTools.TypeByName("Sandbox.Game.Gui.MyTerminalInventoryController");
-            MethodInfo original1 = originalType.GetMethod("SearchInList", BindingFlags.Instance | BindingFlags.NonPublic);
-            MethodInfo postfix1 = typeof(Main).GetMethod("Postfix");
+            Type inventoryController = AccessTools.TypeByName("Sandbox.Game.Gui.MyTerminalInventoryController");
+            MethodInfo original1 = inventoryController.GetMethod("SearchInList", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo postfix1 = typeof(Main).GetMethod("SearchInList");
             harmony.Patch(original1, postfix: new HarmonyMethod(postfix1));
+
+            MethodInfo original2 = inventoryController.GetMethod("ownerControl_InventoryContentsChanged", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo postfix2 = typeof(Main).GetMethod("ownerControl_InventoryContentsChanged");
+            harmony.Patch(original2, postfix: new HarmonyMethod(postfix2));
+
+            searchBoxL = AccessTools.Field(inventoryController, "m_searchBoxLeft");
+            searchBoxR = AccessTools.Field(inventoryController, "m_searchBoxRight");
+            ownersControlL = AccessTools.Field(inventoryController, "m_leftOwnersControl");
+            ownersControlR = AccessTools.Field(inventoryController, "m_rightOwnersControl");
         }
 
         public void Update()
         { }
 
-        public static void Postfix(MyGuiControlTextbox searchText, MyGuiControlList list)
+        public static void ownerControl_InventoryContentsChanged(object __instance, MyGuiControlInventoryOwner control)
         {
-            StringBuilder sb = new StringBuilder();
-            searchText.GetText(sb);
-            string text = sb.ToString();
-            bool emptyText = string.IsNullOrWhiteSpace(text);
-            string[] args = null;
-            if(!emptyText)
-                args = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            MyGuiControlList list = control.Owner as MyGuiControlList;
+            if (list == null)
+                return;
 
+            MyGuiControlSearchBox searchBox;
+            MyGuiControlList otherList = (MyGuiControlList)ownersControlL.GetValue(__instance);
+            if(list == otherList)
+            {
+                searchBox = (MyGuiControlSearchBox)searchBoxL.GetValue(__instance);
+            }
+            else
+            {
+                otherList = (MyGuiControlList)ownersControlR.GetValue(__instance);
+                if(list == otherList)
+                    searchBox = (MyGuiControlSearchBox)searchBoxR.GetValue(__instance);
+                else
+                    return;
+            }
+
+            GetSearch(searchBox.TextBox, out bool emptyText, out string[] args);
+            SearchInOwner(emptyText, args, control);
+        }
+
+        public static void SearchInList(MyGuiControlTextbox searchText, MyGuiControlList list)
+        {
+            GetSearch(searchText, out bool emptyText, out string[] args);
             foreach (MyGuiControlBase control in list.Controls)
             {
                 if (!control.Visible)
@@ -50,25 +76,41 @@ namespace avaness.BetterInventorySearch
                 MyGuiControlInventoryOwner inventoryOwner = control as MyGuiControlInventoryOwner;
                 if (inventoryOwner == null)
                     continue;
-                
-                MyEntity e = inventoryOwner.InventoryOwner;
-                bool noSearch = emptyText || e?.DisplayNameText == null || SearchMatches(args, e.DisplayNameText);
+                SearchInOwner(emptyText, args, inventoryOwner);
+            }
+        }
 
-                foreach (MyGuiControlGrid grid in inventoryOwner.ContentGrids)
+        private static void GetSearch(MyGuiControlTextbox textbox, out bool emptyText, out string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            textbox.GetText(sb);
+            string text = sb.ToString();
+            emptyText = string.IsNullOrWhiteSpace(text);
+            if (!emptyText)
+                args = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            else
+                args = null;
+        }
+
+        private static void SearchInOwner(bool emptyText, string[] args, MyGuiControlInventoryOwner inventoryOwner)
+        {
+            MyEntity e = inventoryOwner.InventoryOwner;
+            bool noSearch = emptyText || e?.DisplayNameText == null || SearchMatches(args, e.DisplayNameText);
+
+            foreach (MyGuiControlGrid grid in inventoryOwner.ContentGrids)
+            {
+                foreach (MyGuiGridItem gridItem in grid.Items)
                 {
-                    foreach (MyGuiGridItem gridItem in grid.Items)
+                    if (noSearch)
                     {
-                        if(noSearch)
-                        {
-                            gridItem.OverlayPercent = 0;
-                        }
-                        else if (gridItem.UserData is MyPhysicalInventoryItem item && TryGetDisplayName(item, out string displayName))
-                        {
-                            if (SearchMatches(args, displayName))
-                                gridItem.OverlayPercent = 0;
-                            else
-                                gridItem.OverlayPercent = 1;
-                        }
+                        gridItem.Enabled = true;
+                    }
+                    else if (gridItem.UserData is MyPhysicalInventoryItem item && TryGetDisplayName(item, out string displayName))
+                    {
+                        if (SearchMatches(args, displayName))
+                            gridItem.Enabled = true;
+                        else
+                            gridItem.Enabled = false;
                     }
                 }
             }
